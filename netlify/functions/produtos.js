ea1bd143181097a5cad655a790fd11826f690ccb
Henrique Melo <hacmelo@gmail.com>
2023-01-17 14:10:32 -0300

    teste

--- netlify/functions/produtos.js
+++ netlify/functions/produtos.js
@@ -1,17 +1,53 @@
-import fetch from "node-fetch";
+const dotenv = require('dotenv');
+const { google } = require('googleapis');
+const sheets = google.sheets({version: 'v4'});
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
+dotenv.config();
 
-export async function handler(event, context) {
+const spreadsheetId = process.env.SHEET_ID;
+const range = process.env.RANGE;
+// const type = process.env.TYPE;
+// const private_key_id = process.env.PRIVATE_KEY_ID;
+const private_key = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+const client_email = process.env.CLIENT_EMAIL;
+// const client_id = process.env.CLIENT_ID;
+const scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+// const credentials = {
+//   type: type,
+//   private_key: private_key,
+//   client_email: client_email,
+//   client_id: client_id,
+//   private_key_id: private_key_id
+// }
+
+const auth = new google.auth.JWT(client_email, null, private_key, scopes);
+
+export async function handler(_event, _context) {
   try {
-    const response = await fetch(API_ENDPOINT);
-    const data = await response.json();
-    return { statusCode: 200, body: JSON.stringify({ data }) };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' }),
-    };
+    const result = await sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: range,
+        auth: auth
+    });
+
+    const data = result.data.values;
+
+    let products = []
+    let headers = data[0]
+    for (let i = 1; i < data.length; i++) {
+        let product = {}
+        let row = data[i]
+        headers.forEach((header, index) => {
+            product[header] = row[index]
+        });
+        products.push(product)
+    }
+
+    // fs.writeFileSync('produtos.json', JSON.stringify(products));
+    res.json(products)
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Error')
   }
 }
